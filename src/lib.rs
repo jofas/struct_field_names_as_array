2071007d4fe3d5523e740c0fb5914226f46b599b
moschroe <moschroe@users.noreply.github.com>
2022-01-19 17:37:58 +0100

    Generate generic comment to satisfy `#[deny(missing_docs)]` on struct type.

--- src/lib.rs
+++ src/lib.rs
@@ -86,6 +86,7 @@ pub fn derive_field_names_as_array(
 
   let result = quote! {
     impl #impl_generics #name #type_generics #where_clause {
+      #[doc=concat!("Generated array of field names for `", stringify!(#name #type_generics), "`.")]
       #vis const FIELD_NAMES_AS_ARRAY: &'static [&'static str] =
         &[#field_names];
     }
