f84a1f10b8243199720909c461c1959b6d40d776
jofas <jonas@fc-web.de>
2023-10-09 14:33:19 +0200

    work on documentation

--- struct-field-names-as-array/src/lib.rs
+++ struct-field-names-as-array/src/lib.rs
@@ -1,4 +1,4 @@
-/// Adds the `FIELD_NAMES_AS_ARRAY` constant to the deriving struct.
+/// Derives the [`FieldNamesAsArray`] trait.
 ///
 /// # Panics
 ///
@@ -24,13 +24,40 @@
 #[cfg(feature = "derive")]
 pub use struct_field_names_as_array_derive::FieldNamesAsArray;
 
+/// Derives the [`FieldNamesAsSlice`] trait.
+///
+/// # Panics
+///
+/// If the token stream is not coming from a named struct or if
+/// the `field_names_as_array` attribute is used wrongfully, deriving
+/// this macro will fail.
+///
+/// # Examples
+///
+/// ```
+/// use struct_field_names_as_array::FieldNamesAsSlice;
+///
+/// #[derive(FieldNamesAsSlice)]
+/// struct Foo {
+///     bar: String,
+///     baz: String,
+///     bat: String,
+/// }
+///
+/// assert_eq!(Foo::FIELD_NAMES_AS_SLICE, ["bar", "baz", "bat"]);
+/// ```
+///
 #[cfg(feature = "derive")]
 pub use struct_field_names_as_array_derive::FieldNamesAsSlice;
 
+/// Exposes the `FIELD_NAMES_AS_ARRAY` constant.
+///
 pub trait FieldNamesAsArray<const N: usize> {
     const FIELD_NAMES_AS_ARRAY: [&'static str; N];
 }
 
+/// Exposes the `FIELD_NAMES_AS_SLICE` constant.
+///
 pub trait FieldNamesAsSlice {
     const FIELD_NAMES_AS_SLICE: &'static [&'static str];
 }
